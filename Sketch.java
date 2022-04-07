900ce7c04987d1eea0de6cdf2549f0b0bb68c789
David <99832335+DavidU28@users.noreply.github.com>
2022-04-07 17:59:50 +0000

    Style

--- Sketch.java
+++ Sketch.java
@@ -31,33 +31,33 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// Grass
-  fill(100, 250 , 50);
-  stroke(0);
-  rect(0, 500, 1000, 1000);
+	  // Grass
+    fill(100, 250 , 50);
+    stroke(0);
+    rect(0, 500, 1000, 1000);
 
-  // Sun
-  fill(255, 255, 0);
-  stroke(0);
-  ellipse(20, 30, 200, 200);
+    // Sun
+    fill(255, 255, 0);
+    stroke(0);
+    ellipse(20, 30, 200, 200);
 
-  // House
-  fill(96, 130, 182);
-  stroke(0);
-  rect(150, 300, 300, 200);
+    // House
+    fill(96, 130, 182);
+    stroke(0);
+    rect(150, 300, 300, 200);
 
-  triangle(150, 300, 450, 300, 300, 200);
-  fill(255, 50, 50);
-  rect(275, 400, 75, 100);
+    triangle(150, 300, 450, 300, 300, 200);
+    fill(255, 50, 50);
+    rect(275, 400, 75, 100);
 
-  fill(0, 0, 0);
-  ellipse(330, 460, 8, 8);
+    fill(0, 0, 0);
+    ellipse(330, 460, 8, 8);
 
-  fill(255);
-  rect(190, 330, 48, 48);
-  fill(0);
-  line(214, 330, 214, 378);
-  line(190, 354, 238, 354);
+    fill(255);
+    rect(190, 330, 48, 48);
+    fill(0);
+    line(214, 330, 214, 378);
+    line(190, 354, 238, 354);
   
   }
   
